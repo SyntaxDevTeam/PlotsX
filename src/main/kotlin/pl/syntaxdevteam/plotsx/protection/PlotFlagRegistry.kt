fe840b54af251315db46a8117352d5c2ddde1a40
SyntaxDevTeam <wieszczy85@gmail.com>
2025-05-30 19:47:34 +0200

    Reorganization of the plot flags

--- src/main/kotlin/pl/syntaxdevteam/plotsx/protection/PlotFlagRegistry.kt
+++ src/main/kotlin/pl/syntaxdevteam/plotsx/protection/PlotFlagRegistry.kt
@@ -12,13 +12,15 @@ object PlotFlagRegistry {
         FlagMeta("button", true, FlagType.BLACKLIST, Material.STONE_BUTTON, "button.name", "button.description"),
         FlagMeta("door", true, FlagType.BLACKLIST, Material.BAMBOO_DOOR, "door.name", "door.description"),
         FlagMeta("smart-door", false, FlagType.WHITELIST, Material.IRON_DOOR, "smart-door.name", "smart-door.description"),
+        FlagMeta("minecart", true, FlagType.BLACKLIST, Material.MINECART, "minecart.name", "minecart.description"),
+        FlagMeta("redstone", false, FlagType.WHITELIST, Material.REDSTONE, "redstone.name", "redstone.description"),
+        FlagMeta("utility", false, FlagType.WHITELIST, Material.FURNACE, "utility.name", "utility.description"),
         FlagMeta("spawn-monsters", false, FlagType.WHITELIST, Material.CARVED_PUMPKIN, "spawn-monsters.name", "spawn-monsters.description"),
         FlagMeta("spawn-animals", true, FlagType.WHITELIST, Material.EGG, "spawn-animals.name", "spawn-animals.description"),
         FlagMeta("passives", false, FlagType.WHITELIST, Material.SADDLE, "passives.name", "passives.description"),
         FlagMeta("flow", true, FlagType.BLACKLIST, Material.WATER_BUCKET, "flow.name", "flow.description"),
         FlagMeta("flow-damage", false, FlagType.WHITELIST, Material.LAVA_BUCKET, "flow-damage.name", "flow-damage.description"),
         FlagMeta("fire", true, FlagType.BLACKLIST, Material.FLINT_AND_STEEL, "fire.name", "fire.description"),
-        FlagMeta("minecart", true, FlagType.BLACKLIST, Material.MINECART, "minecart.name", "minecart.description"),
         FlagMeta("allow-home", false, FlagType.WHITELIST, Material.COMPASS, "allow-home.name", "allow-home.description"),
         FlagMeta("use-potions", false, FlagType.WHITELIST, Material.EXPERIENCE_BOTTLE, "use-potions.name", "use-potions.description"),
         FlagMeta("iceform-player", false, FlagType.WHITELIST, Material.SNOWBALL, "iceform-player.name", "iceform-player.description"),
@@ -28,9 +30,8 @@ object PlotFlagRegistry {
         FlagMeta("allow-spawners", false, FlagType.WHITELIST, Material.SPAWNER, "allow-spawners.name", "allow-spawners.description"),
         FlagMeta("leaves-decay", true, FlagType.BLACKLIST, Material.OAK_LEAVES, "leaves-decay.name", "leaves-decay.description"),
         FlagMeta("effects", true, FlagType.BLACKLIST, Material.BEACON, "effects.name", "effects.description"),
-        FlagMeta("redstone", false, FlagType.WHITELIST, Material.REDSTONE, "redstone.name", "redstone.description"),
-        FlagMeta("utility", false, FlagType.WHITELIST, Material.FURNACE, "utility.name", "utility.description"),
-        FlagMeta("block-transform", true, FlagType.BLACKLIST, Material.MOSS_BLOCK, "block-transform.name", "block-transform.description")
+        FlagMeta("block-transform", true, FlagType.BLACKLIST, Material.MOSS_BLOCK, "block-transform.name", "block-transform.description"),
+        // FlagMeta("block-transform", true, FlagType.BLACKLIST, Material.MOSS_BLOCK, "block-transform.name", "block-transform.description")
         // FlagMeta("team", false, FlagType.WHITELIST, Material.NAME_TAG, "team.name", "team.description"),
         // FlagMeta("mob-loot", false, FlagType.WHITELIST, Material.MYCELIUM, "mob-loot.name", "mob-loot.description"),
         // FlagMeta("allow-fly", false, FlagType.WHITELIST, Material.ELYTRA, "allow-fly.name", "allow-fly.description")
