ed10a69fcf98a30fa6fe7a2ab2d1cc423cc0f782
SyntaxDevTeam <wieszczy85@gmail.com>
2025-07-06 21:20:43 +0200

    Updated libraries and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    kotlin("jvm") version "2.2.0-RC3"
-    id("com.gradleup.shadow") version "9.0.0-beta16"
+    kotlin("jvm") version "2.2.0"
+    id("com.gradleup.shadow") version "9.0.0-rc1"
 }
 
 group = "pl.syntaxdevteam"
-version = "Alpha-1"
+version = "1.0.0-Alpha-4"
 description = ""
 
 repositories {
@@ -24,21 +24,26 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
+    compileOnly("pl.syntaxdevteam:core:1.0.9")
+    compileOnly("pl.syntaxdevteam:cleanerx:1.5.1-DEV")
     compileOnly("org.eclipse.aether:aether-api:1.1.0")
     compileOnly("org.yaml:snakeyaml:2.4")
     compileOnly("com.google.code.gson:gson:2.13.1")
-    compileOnly("net.kyori:adventure-text-serializer-legacy:4.21.0")
-    compileOnly("net.kyori:adventure-text-minimessage:4.21.0")
-    compileOnly("net.kyori:adventure-text-serializer-gson:4.21.0")
-    compileOnly("net.kyori:adventure-text-serializer-plain:4.21.0")
-    compileOnly("net.kyori:adventure-text-serializer-ansi:4.21.0")
+    compileOnly("net.kyori:adventure-text-serializer-legacy:4.23.0")
+    compileOnly("net.kyori:adventure-text-minimessage:4.23.0")
+    compileOnly("net.kyori:adventure-text-serializer-gson:4.23.0")
+    compileOnly("net.kyori:adventure-text-serializer-plain:4.23.0")
+    compileOnly("net.kyori:adventure-text-serializer-ansi:4.23.0")
+    compileOnly("org.mariadb.jdbc:mariadb-java-client:3.5.4")
+    compileOnly("org.xerial:sqlite-jdbc:3.50.2.0")
+    compileOnly("org.postgresql:postgresql:42.7.7")
+    compileOnly("com.h2database:h2:2.3.232")
     compileOnly("com.zaxxer:HikariCP:6.3.0")
     compileOnly("net.luckperms:api:5.5")
     compileOnly("me.clip:placeholderapi:2.11.6")
     compileOnly("io.github.miniplaceholders:miniplaceholders-kotlin-ext:2.3.0")
     compileOnly("com.github.milkbowl:VaultAPI:1.7.1")
-    compileOnly("net.milkbowl.vault:VaultUnlockedAPI:2.11")
-    compileOnly("pl.syntaxdevteam:cleanerx:1.5.1-DEV")
+    compileOnly("net.milkbowl.vault:VaultUnlockedAPI:2.13")
     compileOnly("net.coreprotect:coreprotect:22.4")
 }
 
